5934e45de6be72915084f150ae83247be46880e4
Otlet <otlet@protonmail.com>
2018-06-28 22:13:20 +0200

    Initial commit

---
+++ Assets/Scripts/ScriptMenuButtons.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScriptMenuButtons : MonoBehaviour {
+    
+    public Button btnStartComponent;
+    public Button btnQuitComponent;
+	// Use this for initialization
+	void Start () {
+        Button btnStartOnUI = btnStartComponent.GetComponent<Button>();
+        Button btnQuitOnUI = btnQuitComponent.GetComponent<Button>();
+
+        btnStartOnUI.onClick.AddListener(StartGame);
+        btnQuitOnUI.onClick.AddListener(EndOfGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Labirynth", LoadSceneMode.Single);
+    }
+
+    void EndOfGame()
+    {
+        Application.Quit();
+    }
+}
